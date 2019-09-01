3a7078b05cb483b27994c43d7de915a464cc7277
Azadprog <rehman19942008@hotmail.com>
2019-09-02 01:00:32 +0200

    myNr

--- src/com/temp/testJava01/Main.java
+++ src/com/temp/testJava01/Main.java
@@ -3,5 +3,7 @@ package com.temp.testJava01;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        int myNr = 10 ;
+        System.out.println(myNr);
     }
 }
